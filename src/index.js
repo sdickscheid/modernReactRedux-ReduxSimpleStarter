256257ae1d8a1500cbacf9e544b071f2c8e313fe
sdickscheid <scott.dickscheid@yahoo.com>
2018-05-09 16:18:54 -0700

    initial commit with error for review

--- src/index.js
+++ src/index.js
@@ -8,7 +8,4 @@ const App = function () {
 
 // Take this component's generated HTML and put it on the page (in the DOM)
 
-ReactDOM.render(
-  <App />,
-  document.querySelector('.container')
-);
+ReactDOM.render(<App />, document.querySelector('.container'));
